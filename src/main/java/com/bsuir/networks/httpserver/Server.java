baf27a34dd2c42c8fce831b9914d55fada8b44ea
Dzmitry Tumilovich <=>
2020-02-24 16:07:58 +0300

    some enhancements

--- src/main/java/com/bsuir/networks/httpserver/Server.java
+++ src/main/java/com/bsuir/networks/httpserver/Server.java
@@ -3,18 +3,25 @@ package com.bsuir.networks.httpserver;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+/**
+ * The server class which is an entry point of the program.
+ */
 public class Server {
 
     private int port;
 
     private String directoryFrom;
 
-    public Server(int port, String directoryFrom) {
+    private Server(int port, String directoryFrom) {
         this.port = port;
         this.directoryFrom = directoryFrom;
     }
 
-    void startServer() {
+    /**
+     * Starts the server with the specified {@link Server#port} and in the {@link Server#directoryFrom}.
+     * When server is started it waits for a new connection. Once it happens control is transferred to the {@link ThreadHandler}.
+     */
+    private void startServer() {
         try (var server = new ServerSocket(this.port)) {
             while (true) {
                 //Returns the address of the endpoint this socket is bound to.
@@ -24,12 +31,18 @@ public class Server {
                 thread.start();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println(e.getMessage());
         }
     }
 
+    /**
+     * Entry point of the program.
+     * Required arguments (and its order):
+     * 1 - port on which server will be started. Should be an integer value.
+     * 2 - folder name ???
+     * @param args - an array of cmd arguments.
+     */
     public static void main(String[] args) {
-        //первый аргумент cmd - порт, второй - папка
         var port = Integer.parseInt(args[0]);
         var directory = args[1];
         new Server(port, directory).startServer();
--- src/main/java/com/bsuir/networks/httpserver/ThreadHandler.java
+++ src/main/java/com/bsuir/networks/httpserver/ThreadHandler.java
@@ -1,22 +1,23 @@
 package com.bsuir.networks.httpserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
+import com.bsuir.networks.httpserver.exception.BadRequestException;
+import com.bsuir.networks.httpserver.exception.NotFoundException;
+
+import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class ThreadHandler extends Thread {
 
-    private static String NOT_FOUND = "NOT FOUND";
+    private static final List<String> REQUEST_METHODS = List.of("GET");
 
     //map for response
-    public static final Map<String,String> CONTENT_TYPES = new HashMap<>() {{
+    private static final Map<String,String> CONTENT_TYPES = new HashMap<>() {{
         put("jpg", "image/jpeg");
         put("html", "text/html");
         put("json", "application/json");
@@ -31,32 +32,61 @@ public class ThreadHandler extends Thread {
 
     ThreadHandler(Socket socket, String directoryPath) {
         this.socket = socket;
-        this.directoryPath=directoryPath;
+        this.directoryPath = directoryPath;
     }
 
     @Override
     public void run() {
-        try (
-            var input = this.socket.getInputStream();
-            var output = this.socket.getOutputStream()){
-            var url = getRequestUrl(input);
+//        System.out.println("New connection established");
+//        System.out.println(String.format("Remote address: %s", socket.getRemoteSocketAddress()));
+
+        InputStream input = null;
+        OutputStream output = null;
+
+        try {
+            input = this.socket.getInputStream();
+            output = this.socket.getOutputStream();
+
+            var requestLine = this.getRequestLine(input);
+            if (!validateRequest(requestLine)) {
+                throw new BadRequestException(String.format("Method is not supported! Available methods: %s", String.join(", ", REQUEST_METHODS)));
+            }
+
+            var url = this.getRequestUrl(requestLine);
             var filePath = Path.of(this.directoryPath + url);
             if (Files.exists(filePath) && !Files.isDirectory(filePath)) {
                 var extension = getFileExtension(filePath);
                 var type = CONTENT_TYPES.get(extension);
                 var fileBytes = Files.readAllBytes(filePath);
-                this.buildHeader(output, 200, "OK", type, fileBytes.length);
-                output.write(fileBytes);
-            }else {
-                //отправляем 404
-                var contentType = CONTENT_TYPES.get("text");
-                this.buildHeader(output, 404, "Not Found", contentType, NOT_FOUND.length());
-                output.write(NOT_FOUND.getBytes());
+                this.sendResponse(output, 200, "OK", type, fileBytes);
+            } else {
+                throw new NotFoundException("Unable to access specified resource");
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (NotFoundException e) {
+            var contentType = CONTENT_TYPES.get("txt");
+            this.sendResponse(output, 404, "Not Found", contentType, e.getMessage().getBytes());
+        } catch (Exception e) {
+            var contentType = CONTENT_TYPES.get("txt");
+            this.sendResponse(output, 400, "Bad Request", contentType, e.getMessage().getBytes());
+        } finally {
+            this.closeStream(input);
+            this.closeStream(output);
         }
+    }
+
+    private String getRequestLine(InputStream inputStream) {
+        var scanner = new Scanner(inputStream).useDelimiter("\r\n");
+        return scanner.next();
+    }
+
+    private boolean validateRequest(String requestLine) {
+        var httpMethod = requestLine.split(" ")[0];
+        return REQUEST_METHODS.contains(httpMethod);
+    }
+
+    private String getRequestUrl(String requestLine) {
+        return requestLine.split(" ")[1];
     }
 
     private String getFileExtension(Path path) {
@@ -67,19 +97,32 @@ public class ThreadHandler extends Thread {
         return extensionStart == -1 ? "" : name.substring(extensionStart + 1);
     }
 
-    private void buildHeader(OutputStream output, int statusCode, String statusText, String type, long lenght) {
+    private void sendHeader(OutputStream output, int statusCode, String statusText, String type, long length) {
         var ps = new PrintStream(output);
         ps.printf("HTTP/1.1 %s %s%n", statusCode, statusText);
         ps.printf("Content-Type: %s%n", type);
-        ps.printf("Content-Length: %s%n%n", lenght);
+        ps.printf("Content-Length: %s%n%n", length);
     }
 
-    private String getRequestUrl(InputStream inputStream) {
-        var scanner = new Scanner(inputStream).useDelimiter("\r\n");
-        var startingLine = scanner.next();
-        // разделяю по пробелу и возвращаю второй элемент,
-        // т к второй элемент url - GET /path HTTP/1.1
-        return startingLine.split(" ")[1];
+    private void sendResponse(OutputStream output, int statusCode, String statusText, String contentType, byte[] responseBody) {
+        this.sendHeader(output, statusCode, statusText, contentType, responseBody.length);
+        try {
+            output.write(responseBody);
+        } catch (IOException e) {
+            System.err.println("An error occurred while sending response!");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void closeStream(Closeable stream) {
+        try {
+            if (stream != null) {
+                stream.close();
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred while closing connection!");
+            System.err.println(e.getMessage());
+        }
     }
 
 }
